4364631199e708c41e5e5d0cb5ea49495af2dd87
ancientmodern <ancientmodern@sjtu.edu.cn>
2021-12-12 00:05:12 +0800

    change color

--- public/js/cfColor.js
+++ public/js/cfColor.js
@@ -4,9 +4,9 @@ define("cfColor", [], function() {
             if (score === "unrated" || score < 1000) return "gray";
             if (score < 1500) return "black";
             if (score < 2000) return "green";
-            if (score < 2250) return "#75dfbb";
-            if (score < 2500) return "#aaabfe";
-            if (score < 2800) return "purple";
+            if (score < 2300) return "#75dfbb";
+            if (score < 2600) return "#aaabfe";
+            if (score < 2850) return "purple";
             if (score < 3000) return "orange";
             return "red";
         }
